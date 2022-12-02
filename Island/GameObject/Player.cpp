398fb512db0497ecc9bf3d8c7f34db9e3fbb2a50
sesbat <sesbat0174@gmail.com>
2022-12-02 15:03:32 +0900

    merge

--- Island/GameObject/Player.cpp
+++ Island/GameObject/Player.cpp
@@ -20,7 +20,7 @@
 
 
 Player::Player()
-	: currState(States::None), speed(2000.f), maxSpeed(5000.f),
+	: currState(States::None), speed(200.f), maxSpeed(200.f),
 	look(1.f, 0.f), prevLook(1.f, 0.f),
 	direction(1.f, 0.f), lastDirection(1.f, 0.f),
 	hp(500), maxHp(500), isDash(false), stamina(10.f), maxStamina(10.f),
